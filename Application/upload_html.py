16dab2b20797d053fc73f969d1a786ba0cecb711
EC2 Default User <ec2-user@ip-172-31-13-166.eu-central-1.compute.internal>
2021-08-30 12:24:10 +0000

    alp

--- Application/upload_html.py
+++ Application/upload_html.py
@@ -1,14 +1,5 @@
 import boto3
 
-#s3 = boto3.resource('s3')
-#s3.meta.client.upload_file('Application/index.html', 's3-website-nv.com', 'index.html')
-#s3.meta.client.upload_file('Application/error.html', 's3-website-nv.com', 'error.html')
-
-s3 = boto3.client('s3')
-
-filename = ['Application/index.html','Application/error.html']
-bucket_name = 's3-website-nv.com'
-
-for file in filename:
-    data = open(file)   
-    s3.put_object(Body=data,Bucket='s3-website-nv.com',Key=file,ContentType='text/html')
+s3 = boto3.resource('s3')
+s3.meta.client.upload_file('Application/index.html', 's3-website-nv.com', 'index.html'.ExtraArgs={'ContentType':'text/html'})
+s3.meta.client.upload_file('Application/error.html', 's3-website-nv.com', 'error.html',ExtraArgs={'ContentType':'text/html'})
