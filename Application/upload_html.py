6124446b1e354a1d50ec3c6907ba025a6e1de76f
EC2 Default User <ec2-user@ip-172-31-13-166.eu-central-1.compute.internal>
2021-08-30 11:01:37 +0000

    added in upload_html.py

--- Application/upload_html.py
+++ Application/upload_html.py
@@ -1,5 +1,5 @@
 import boto3
 
 s3 = boto3.resource('s3')
-s3.meta.client.upload_file('index.html', 's3-website-nv.com', 'index.html')
-s3.meta.client.upload_file('error.html', 's3-website-nv.com', 'error.html')
+s3.meta.client.upload_file('Application/index.html', 's3-website-nv.com', 'index.html')
+s3.meta.client.upload_file('Application/error.html', 's3-website-nv.com', 'error.html')
