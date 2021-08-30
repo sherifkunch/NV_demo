c6ac3e25e2ab9fe8386a6852f0f7c358cb7cfd98
EC2 Default User <ec2-user@ip-172-31-13-166.eu-central-1.compute.internal>
2021-08-30 12:11:42 +0000

    added new way of ulpadhtml

--- Application/upload_html.py
+++ Application/upload_html.py
@@ -1,5 +1,17 @@
 import boto3
 
-s3 = boto3.resource('s3')
-s3.meta.client.upload_file('Application/index.html', 's3-website-nv.com', 'index.html')
-s3.meta.client.upload_file('Application/error.html', 's3-website-nv.com', 'error.html')
+#s3 = boto3.resource('s3')
+#s3.meta.client.upload_file('Application/index.html', 's3-website-nv.com', 'index.html')
+#s3.meta.client.upload_file('Application/error.html', 's3-website-nv.com', 'error.html')
+
+s3 = boto3.client('s3')
+
+ filename = ['index.html','error.html']
+ bucket_name = 's3-website-nv.com'
+
+ for file in filename:
+         data = open(file)
+         s3.put_object(  Body=data,
+                         Bucket='s3-website-nv.com',
+                         Key=file,
+                         ContentType='text/html' )
